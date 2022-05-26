5f301a8fcd8600ebd2bbe9d370ae61f05fb4de61
Joseph Broome <josephlloydbroome@gmail.com>
2022-05-26 14:08:16 -0700

    JoeProject File

---
+++ JoeProject/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine($"Hi. Enter your name bellow");
+var teacherName = Console.ReadLine();
+
+Console.WriteLine($"Hello, {teacherName}!  How are you today?");
